c1080356409c0f75831bcf7a3b4b954e5cdff224
Zavalnyuk David <144224528+Dvadd@users.noreply.github.com>
2023-11-29 20:20:44 +0200

    Add files via upload

---
+++ M1-main/Python/M1/L1/Bonus/t1.py
@@ -0,0 +1,7 @@
+'''
+#TODO Программа має надрукувати "This is Python. It’s cool! I really like programming! It’s cool!".
+
+За допомогою аргумента format відформатуй рядок так, щоб він мав наступний вигляд:
+'''
+txt = "This is {language}. It’s {adj}! I {how} like programming! It’s {adj}!"
+print(txt.format(language = "Python", adj = "cool", how = "really"))
---
+++ M1-main/Python/M1/L1/Bonus/t2.py
@@ -0,0 +1,8 @@
+'''
+Вигадай діалог.
+Вигадай діалог і виведи його на екран, використовуючи функцію print ().
+'''
+print("Knock-knock!")
+name = input("Who is there?") 
+repeat = input(name + " who?")
+print("Come in, " + repeat + ".")
